306117ad4b1376d58d254f3e38227a3f52eb4f49
PythonicIconic <67925095+PythonicIconic@users.noreply.github.com>
2020-10-23 16:02:34 -0500

    Added scrape option with optional filter

--- main.go
+++ main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"bufio"
 	"encoding/xml"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 var (
 	assetURI = regexp.MustCompile(`(data-mediathumb-url=")(.*)(")`)
-	wg       = &sync.WaitGroup{}
+	mainwg   = &sync.WaitGroup{}
+	inputwg  = &sync.WaitGroup{}
 )
 
 type Asset struct {
@@ -54,7 +58,8 @@ type Content struct {
 	URL     string   `xml:"url"`
 }
 
-func GetAssetType(id string) (assetType string, trueID string) {
+func GetAssetType(id string, assetChan chan [2]string) {
+	defer inputwg.Done()
 	resp, err := http.Get(fmt.Sprintf("https://assetdelivery.roblox.com/v1/asset?id=%v", id))
 	if err != nil {
 		fmt.Println("[ERROR] Failed to make request:", err)
@@ -73,33 +78,45 @@ func GetAssetType(id string) (assetType string, trueID string) {
 	if err != nil {
 		if err.Error() == "EOF" {
 			fmt.Println("[WARNING] Given ID was not found, assuming it is audio")
-			wg.Add(1)
+			mainwg.Add(1)
 			go Audio(id)
 
 			return
+		} else if err.Error() == "XML syntax error on line 3: invalid character entity & (no semicolon)" {
+			fmt.Println("[ERROR] Old format detected, skipping")
+			return
 		} else {
-			fmt.Println("[ERROR] Unknown error occurred getting asset info:", err)
-
+			fmt.Println("[ERROR] Unknown error occurred getting asset info:", err, "ID:", id)
 			return
 		}
 	}
 
-	if asset.Item.Class == "Accessory" {
-		wg.Add(1)
+	switch asset.Item.Class {
+	case "Accessory":
+		mainwg.Add(1)
 		go Accessory(id, asset.Item.Properties.Name)
 
 		return
-	} else if asset.Item.Class == "Folder" {
-		wg.Add(1)
+	case "Folder":
+		mainwg.Add(1)
 		go Accessory(strings.Split(asset.Item.NestedItem.AnimItem.Properties.Content.URL, "id=")[1], "UNKNOWN")
 
 		return
+	case "Lighting":
+		mainwg.Add(1)
+		go Accessory(strings.Split(asset.Item.NestedItem.Properties.Content.URL, "id=")[1], "UNKNOWN")
+
+		return
+	case "ControllerService", "Model", "Teams", "Timer", "Part":
+		return
 	}
 
-	assetType = asset.Item.Class
-	trueID = strings.Split(asset.Item.Properties.Content.URL, "id=")[1]
+	if len(strings.Split(asset.Item.Properties.Content.URL, "id=")) < 2 {
+		fmt.Println("[ERROR] Unknown error occurred when retrieving URL. Unsupported asset?")
+		return
+	}
 
-	return
+	assetChan <- [2]string{asset.Item.Class, strings.Split(asset.Item.Properties.Content.URL, "id=")[1]}
 }
 
 func DownloadAsset(id string) (data []byte) {
@@ -120,7 +137,7 @@ func DownloadAsset(id string) (data []byte) {
 }
 
 func Accessory(id string, assetName string) {
-	defer wg.Done()
+	defer mainwg.Done()
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://assetdelivery.roblox.com/v1/asset?id=%v", id), nil)
 	if err != nil {
@@ -153,7 +170,7 @@ func Accessory(id string, assetName string) {
 }
 
 func Audio(id string) {
-	defer wg.Done()
+	defer mainwg.Done()
 	resp, err := http.Get(fmt.Sprintf("https://roblox.com/library/%v/#", id))
 	if err != nil {
 		fmt.Println("[ERROR] Failed to create request:", err)
@@ -168,6 +185,10 @@ func Audio(id string) {
 	resp.Body.Close()
 
 	elem := assetURI.Find(body)
+	if elem == nil {
+		fmt.Println("[ERROR] Failed to find audio URL, invalid ID")
+		return
+	}
 
 	url := strings.Split(string(elem), "\"")[1]
 	resp, err = http.Get(url)
@@ -192,7 +213,7 @@ func Audio(id string) {
 }
 
 func Shirt(id string) {
-	defer wg.Done()
+	defer mainwg.Done()
 	body := DownloadAsset(id)
 
 	err := ioutil.WriteFile(fmt.Sprintf("shirts/%v.png", id), body, 0777)
@@ -205,7 +226,7 @@ func Shirt(id string) {
 }
 
 func Pants(id string) {
-	defer wg.Done()
+	defer mainwg.Done()
 	body := DownloadAsset(id)
 
 	err := ioutil.WriteFile(fmt.Sprintf("pants/%v.png", id), body, 0777)
@@ -218,7 +239,7 @@ func Pants(id string) {
 }
 
 func Tshirt(id string) {
-	defer wg.Done()
+	defer mainwg.Done()
 	body := DownloadAsset(id)
 
 	err := ioutil.WriteFile(fmt.Sprintf("tshirts/%v.png", id), body, 0777)
@@ -231,7 +252,7 @@ func Tshirt(id string) {
 }
 
 func Face(id string) {
-	defer wg.Done()
+	defer mainwg.Done()
 	body := DownloadAsset(id)
 
 	err := ioutil.WriteFile(fmt.Sprintf("faces/%v.png", id), body, 0777)
@@ -243,7 +264,50 @@ func Face(id string) {
 	fmt.Println("[SUCCESS] Saved face", id)
 }
 
-func main() {
+func Scrape(filter string) {
+	if filter == "Tshirt" {
+		filter = "ShirtGraphic"
+	} else if filter == "Face" {
+		filter = "Decal"
+	}
+
+	assetChan := make(chan [2]string, 200)
+	go func() {
+		for id := 1000000; id < 5999999999; id++ {
+			inputwg.Add(1)
+			go GetAssetType(strconv.Itoa(id), assetChan)
+			time.Sleep(50 * time.Millisecond)
+		}
+
+		inputwg.Wait()
+		close(assetChan)
+	}()
+
+	for assetInfo := range assetChan {
+		if assetInfo[0] == filter || filter == "" {
+			switch assetInfo[0] {
+			case "Shirt":
+				mainwg.Add(1)
+				go Shirt(assetInfo[1])
+			case "Pants":
+				mainwg.Add(1)
+				go Pants(assetInfo[1])
+			case "ShirtGraphic":
+				mainwg.Add(1)
+				go Tshirt(assetInfo[1])
+			case "Decal":
+				mainwg.Add(1)
+				go Face(assetInfo[1])
+			}
+		} else {
+			continue
+		}
+	}
+
+	mainwg.Wait()
+}
+
+func File() {
 	file, err := os.Open("assets.txt")
 	if err != nil {
 		fmt.Println("[ERROR] Failed to open assets.txt. Are you in the right folder?")
@@ -251,6 +315,39 @@ func main() {
 	}
 	defer file.Close()
 
+	assetChan := make(chan [2]string, 20)
+	go func() {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			inputwg.Add(1)
+			go GetAssetType(scanner.Text(), assetChan)
+		}
+
+		inputwg.Wait()
+		close(assetChan)
+	}()
+
+	for assetInfo := range assetChan {
+		switch assetInfo[0] {
+		case "Shirt":
+			mainwg.Add(1)
+			go Shirt(assetInfo[1])
+		case "Pants":
+			mainwg.Add(1)
+			go Pants(assetInfo[1])
+		case "ShirtGraphic":
+			mainwg.Add(1)
+			go Tshirt(assetInfo[1])
+		case "Decal":
+			mainwg.Add(1)
+			go Face(assetInfo[1])
+		}
+	}
+
+	mainwg.Wait()
+}
+
+func main() {
 	_ = os.Mkdir("shirts", 0777)
 	_ = os.Mkdir("pants", 0777)
 	_ = os.Mkdir("audio", 0777)
@@ -258,24 +355,38 @@ func main() {
 	_ = os.Mkdir("faces", 0777)
 	_ = os.Mkdir("accessories", 0777)
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		assetType, trueID := GetAssetType(scanner.Text())
-		switch assetType {
-		case "Shirt":
-			wg.Add(1)
-			go Shirt(trueID)
-		case "Pants":
-			wg.Add(1)
-			go Pants(trueID)
-		case "ShirtGraphic":
-			wg.Add(1)
-			go Tshirt(trueID)
-		case "Decal":
-			wg.Add(1)
-			go Face(trueID)
-		}
-	}
+	var (
+		scrape bool
+		file   bool
+		filter string
+	)
 
-	wg.Wait()
+	flag.StringVar(&filter, "filter", "", "Set a specific asset type to scrape for. Only works with scrape.\nSupported filters: Shirt, Pants, Tshirt, Face, Audio, Accessory")
+	flag.BoolVar(&scrape, "scrape", false, "Set whether to scrape assets or not.")
+	flag.BoolVar(&file, "file", false, "Set whether to use file or not.")
+	flag.Parse()
+
+	if scrape {
+		if filter != "" {
+			var match bool
+			filter = strings.Title(strings.ToLower(filter))
+			for _, asset := range []string{"Shirt", "Pants", "Tshirt", "Face", "Audio", "Accessory"} {
+				if filter == asset {
+					match = true
+				}
+			}
+
+			if match {
+				Scrape(filter)
+			} else {
+				fmt.Println("Invalid filter! Supported filters: Shirt, Pants, Tshirt, Face, Audio, Accessory")
+			}
+		} else {
+			Scrape("")
+		}
+	} else if file {
+		File()
+	} else {
+		fmt.Println("Oops! No option selected. Please use `massetdl -scrape` or `massetdl -file`!")
+	}
 }
